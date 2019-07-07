8bc100dc57eef5d7c0c57296c231c62fc542e576
Slug <xwffirilat@gmail.com>
2019-07-07 16:10:43 -0500

    mass-format code, add main functions

--- Encrypt.py
+++ Encrypt.py
@@ -1,28 +1,33 @@
 def encrypt(s, e, n):
     out = []
     for x in s:
-        out.append(modde(ord(x),e,n))
-    return(out)
-    
+        out.append(modde(ord(x), e, n))
+    return (out)
+
+
 def modde(m, dee, en):
     M = m
-    for i in range(0,dee -1):
-        M = (M*m)%en
+    for i in range(0, dee - 1):
+        M = (M * m) % en
     return M
 
-a = True
 
-while a:
-    kom = input("Enter a message here (or enter \"quit\" to quit): ")
-    N = input("Enter an n value: ")
-    while not N.isnumeric():
-        print("Please enter an integer value")
+def main():
+    a = True
+
+    while a:
+        kom = input("Enter a message here (or enter \"quit\" to quit): ")
         N = input("Enter an n value: ")
-    N = int(N)
-    E = input("Enter an e value: ")
-    while not E.isnumeric():
-        print("Please enter an integer value")
+        while not N.isnumeric():
+            print("Please enter an integer value")
+            N = input("Enter an n value: ")
+        N = int(N)
         E = input("Enter an e value: ")
-    E = int(E)
-    print(encrypt(kom, E, N))
-    
+        while not E.isnumeric():
+            print("Please enter an integer value")
+            E = input("Enter an e value: ")
+        E = int(E)
+        print(encrypt(kom, E, N))
+
+if __name__ == '__main__':
+    main()
--- Encrypt3.py
+++ Encrypt3.py
@@ -1,18 +1,19 @@
 def encrypt(s, e, n):
     out = []
-    while len(s)%3!=0:
-            s+="#"
-    for i in range(0,int(len(s)/3)):
-        st = s[3*i:3*i+3]
-        a,b,c = str(ord(st[0])),str(ord(st[1])),str(ord(st[2]))
-        while len(b)!=3:
+    while len(s) % 3 != 0:
+        s += "#"
+    for i in range(0, int(len(s) / 3)):
+        st = s[3 * i:3 * i + 3]
+        a, b, c = str(ord(st[0])), str(ord(st[1])), str(ord(st[2]))
+        while len(b) != 3:
             b = "0" + b
-        while len(c)!=3:
+        while len(c) != 3:
             c = "0" + c
         k = int(a + b + c)
-        out.append(modde(k,e,n))
-    return(out)
-    
+        out.append(modde(k, e, n))
+    return (out)
+
+
 def modde(m, dee, en):
     x = str(bin(dee)).lstrip("0b")
     x = x[::-1]
@@ -21,29 +22,34 @@ def modde(m, dee, en):
         b.append(int(x[i]))
     sq = [m]
     for i in range(1, len(x)):
-        sq.append((sq[i-1]**2)%en)
+        sq.append((sq[i - 1] ** 2) % en)
     cu = 1
-    for i in range(0,len(x)):
+    for i in range(0, len(x)):
         if b[i] == 1:
-            cu = (cu*sq[i])%en
-        
-    #M = m
-    #for i in range(0,dee -1):
+            cu = (cu * sq[i]) % en
+
+    # M = m
+    # for i in range(0,dee -1):
     #    M = (M*m)%en
     return cu
 
-a = True
+def main():
+    a = True
 
-while a:
-    kom = input("Enter a message here (or enter \"quit\" to quit): ")
-    N = input("Enter an n value: ")
-    while not N.isnumeric():
-        print("Please enter an integer value")
+    while a:
+        kom = input("Enter a message here (or enter \"quit\" to quit): ")
         N = input("Enter an n value: ")
-    N = int(N)
-    E = input("Enter an e value: ")
-    while not E.isnumeric():
-        print("Please enter an integer value")
+        while not N.isnumeric():
+            print("Please enter an integer value")
+            N = input("Enter an n value: ")
+        N = int(N)
         E = input("Enter an e value: ")
-    E = int(E)
-    print(encrypt(kom, E, N))
+        while not E.isnumeric():
+            print("Please enter an integer value")
+            E = input("Enter an e value: ")
+        E = int(E)
+        print(encrypt(kom, E, N))
+
+
+if __name__ == '__main__':
+    main()
